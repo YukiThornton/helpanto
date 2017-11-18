697cea7b59abb440d8c0f485bca7f7e75e43da63
okunoyuki <yukiokuno16@gmail.com>
2017-11-18 19:35:36 +0900

    Change styles and icon.

--- src/components/RecipeRow.js
+++ src/components/RecipeRow.js
@@ -1,11 +1,13 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
+import '../styles/App.css';
 
 const RecipeRow = ({onClick, title, selected}) => (
   <li
     onClick={onClick}
+    className="Clickable"
     style={{
-      color: selected ? 'green' : 'gray'
+      color: selected ? '#0097A7' : 'gray'
     }}
   >
     {title}
